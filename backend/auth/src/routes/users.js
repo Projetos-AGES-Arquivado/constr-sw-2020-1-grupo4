75de9d44e067ae1b7a4b3d5926b0288ac9d5f5a2
Giuseppe Menti <mentifg@gmail.com>
2020-05-11 19:54:20 -0300

    Add list and get user

--- backend/auth/src/routes/users.js
+++ backend/auth/src/routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { DuplicatedResourceError } = require('../errors');
+const { DuplicatedResourceError, ResourceNotFoundError } = require('../errors');
 const validatorMiddleware = require('../middlewares/validator');
 const createUserSchema = require('../schemas/create-user-schema');
 const { User, Role } = require('../models');
@@ -47,5 +47,34 @@ router.post(
     }
   }
 );
+
+router.get('/', async (req, res, next) => {
+  try {
+    res.send(await User.find());
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      throw new ResourceNotFoundError();
+    }
+    res.send(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await User.deleteOne({ _id: req.params.id });
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
