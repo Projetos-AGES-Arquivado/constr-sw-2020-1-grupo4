b097d6242759220bf0ca49f58528fcbc0a645544
CarlosAsrc <carlos.asrc@gmail.com>
2020-05-12 20:32:45 -0300

    Remove configuraçao desnecessario do mongo no docker-compose.

--- backend/auth/src/models/index.js
+++ backend/auth/src/models/index.js
@@ -5,8 +5,7 @@ const Role = require('./role');
 process.env.MONGO_CONNECTION = 'mongodb+srv://user:Ud3QU7wz0T218FLQ@cluster0-pfhhf.mongodb.net/test';
 
 mongoose.connect(
-  process.env.MONGO_CONNECTION ||
-    'mongodb://mongo:27017'
+  process.env.MONGO_CONNECTION
 );
 
 module.exports = { User, Role };
