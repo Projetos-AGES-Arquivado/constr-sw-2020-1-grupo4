5fbf24db50002b5807f1ae22875b340522294b76
CarlosAsrc <carlos.asrc@gmail.com>
2020-05-13 14:17:52 -0300

    Adiciona documentaçao da API com Swagger

--- backend/auth/src/index.js
+++ backend/auth/src/index.js
@@ -4,12 +4,16 @@ const usersRouter = require('./routes/users');
 const rolesRouter = require('./routes/roles');
 const errHandlerMiddleware = require('./middlewares/errHandler');
 const { NotFoundError } = require('./errors');
+const swaggerUi = require('swagger-ui-express');
+const openApiDocumentation = require('./swagger/swagger.json');
+
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/users', usersRouter);
 app.use('/api/roles', rolesRouter);
+app.use('/api', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 
 app.get('/ping', function (req, res) {
   res.send('pong');
