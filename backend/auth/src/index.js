941883175b0b1b3b8ce0f9b7578ea82375249cec
ilegra.rodrigues <ilegra.rodrigues>
2020-06-15 20:47:10 -0300

    Add cors origin on headers

--- backend/auth/src/index.js
+++ backend/auth/src/index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Header", "*"); 
+  res.header("Access-Control-Allow-Headers", "*"); 
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,PATCH');
   app.use(cors());
